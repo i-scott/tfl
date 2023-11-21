ddb5cc272d12e107782b9cb0cd21854ca724f1a5
ScoRoScuba <scott.roberts@virgin.net>
2023-11-21 20:16:15 +0000

    Initial Commit

---
+++ RoadStatus/RoadStatus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoadStatus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
