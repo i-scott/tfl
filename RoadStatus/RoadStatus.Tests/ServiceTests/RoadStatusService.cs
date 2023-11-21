2b8a8321d36db308f1e233abfd45d8b600372b79
ScoRoScuba <scott.roberts@virgin.net>
2023-11-21 20:17:38 +0000

    Added in initial RoadStatusService Tests

---
+++ RoadStatus/RoadStatus.Tests/ServiceTests/RoadStatusService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RoadStatus.Tests.ServiceTests
+{
+    public class RoadStatusService
+    {
+        public RoadStatusService()
+        {
+        }
+
+        public async Task<string> RunAsync(string v)
+        {
+            return v == "A2" ? v : "The following road is not recognised: A2";
+        }
+    }
+}
---
+++ RoadStatus/RoadStatus.Tests/ServiceTests/RoadStatusServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RoadStatus.Tests.ServiceTests
+{
+    public class RoadStatusServiceTests
+    {
+        [Fact]
+        public async void WhenGivenValidRoadID_DisplayNameIsReturned()
+        {
+            var sut = new RoadStatusService();
+
+            var result = await sut.RunAsync("A2");
+
+            result.Should().Be("A2");
+        }
+
+
+        [Fact]
+        public async void WhenGivenInvalidRoadID_RoadNotRecognisedReturned()
+        {
+            var sut = new RoadStatusService();
+
+            var result = await sut.RunAsync("B3");
+
+            result.Should().Be("The following road is not recognised: A2");
+        }
+    }
+}
