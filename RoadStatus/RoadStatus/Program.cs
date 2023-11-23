ffa6141b6f5ad450a037a26e5d2ccc67185dc1fc
ScoRoScuba <scott.roberts@virgin.net>
2023-11-23 21:39:21 +0000

    Started to wire up the app

--- RoadStatus/RoadStatus/Program.cs
+++ RoadStatus/RoadStatus/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 
 namespace RoadStatus
 {
@@ -6,6 +10,24 @@ namespace RoadStatus
     {
         static void Main(string[] args)
         {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var tflSection = config.GetSection("tfl");
+            var tflSecuritySection = tflSection.GetSection("secuirty");
+            var tflBaseUrl = tflSection.GetValue<string>("baseUrl");
+
+            var tflAppID = tflSecuritySection.GetValue<string>("appId");
+            var tflAppKey = tflSecuritySection.GetValue<string>("primaryAppKey");
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder => builder.AddConsole())                    
+                .BuildServiceProvider();
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
